f3a9d726d212dbc53516dabb350dfcf7250c3911
PKui <1321095399@qq.com>
2020-01-08 17:21:05 +0800

    扩展axios.create静态接口

--- src/axios.ts
+++ src/axios.ts
@@ -1,21 +1,24 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/utils'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-console.log(defaults)
+// console.log(defaults)
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   // 给 request 内部绑定this
   const instance = Axios.prototype.request.bind(context)
   // const instance = Axios.prototype.request
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
 // console.dir(axios)
 // console.dir(axios.get('get').then(res => console.log(res)))
+
+axios.create = (config: AxiosRequestConfig) => createInstance(mergeConfig(defaults, config))
 
 export default axios
